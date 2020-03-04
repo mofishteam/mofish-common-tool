62269d4f63fe0c471e2a8538dc9951667c68bd68
wszxdhr <wszxdhr@126.com>
2020-03-04 08:06:12 +0800

    feat:添加测试脚本

---
+++ test/demo.js
@@ -0,0 +1,9 @@
+const should = require('chai').should()
+
+describe('Hello World!', () => {
+  describe('Oh My God!', () => {
+    it('it is a shame!', () => {
+      'abc'.should.be.a('string')
+    })
+  })
+})
